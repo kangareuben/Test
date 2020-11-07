defaf59a60a1cd689c49ca9ee6ccc9844614a3d5
Reuben Brenner-Adams <rmb1952@rit.edu>
2020-11-07 13:44:14 -0500

    Added initial Unity project
    Template from ShapeOutlineGame

---
+++ Unity Project/Shape Outline Game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	private Vector2 mousePos;
+	private Vector2 prevMousePos;
+
+	public GameObject line;
+	private LineRenderer currentLine;
+	private List<Vector3> points;
+
+	// Use this for initialization
+	void Start()
+	{
+		points = new List<Vector3>();
+		InvokeRepeating("HandleInput", 0f, .05f);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			GameObject l = Instantiate(line, transform.position, Quaternion.identity) as GameObject;
+			currentLine = l.GetComponent<LineRenderer>();
+			points.Clear();
+		}
+	}
+
+	private void HandleInput()
+	{
+		mousePos = Input.mousePosition;
+
+		if(Input.GetMouseButton(0) && (mousePos != prevMousePos))
+		{
+			points.Add(Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f)));
+			currentLine.positionCount = points.Count;
+			currentLine.SetPosition(points.Count - 1, points[points.Count - 1]);
+		}
+
+		prevMousePos = mousePos;
+	}
+}
